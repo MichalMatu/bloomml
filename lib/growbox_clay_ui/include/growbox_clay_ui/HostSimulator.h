4f3ebe2a5e5761b43064865d1279328bfd1025ed
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-20 19:28:04 +0200

    Reuse shared Clay page renderer in host API

--- lib/growbox_clay_ui/include/growbox_clay_ui/HostSimulator.h
+++ lib/growbox_clay_ui/include/growbox_clay_ui/HostSimulator.h
@@ -1,29 +1,16 @@
 #pragma once
 
-#include "growbox_display_model/DisplayPageModel.h"
+#include "growbox_clay_ui/PageRenderer.h"
 
 #include <array>
-#include <cstddef>
 #include <cstdint>
 
 namespace growbox::clay_ui {
-
-inline constexpr std::uint16_t kDisplayWidth = 296;
-inline constexpr std::uint16_t kDisplayHeight = 128;
-inline constexpr std::size_t kFrameStrideBytes = (kDisplayWidth + 7U) / 8U;
-inline constexpr std::size_t kFrameBytes = kFrameStrideBytes * kDisplayHeight;
 
 struct MonochromeFrame {
   std::array<std::uint8_t, kFrameBytes> bytes{};
 
   [[nodiscard]] bool pixel(std::uint16_t x, std::uint16_t y) const noexcept;
-};
-
-struct RenderSummary {
-  std::uint16_t width = 0;
-  std::uint16_t height = 0;
-  std::size_t black_pixels = 0;
-  std::size_t render_commands = 0;
 };
 
 [[nodiscard]] bool renderHostSmoke(MonochromeFrame& frame,
