e2730dc7f11cc0b3def0fca654319d9c803a9cf8
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-20 19:30:03 +0200

    Add persistent Clay arena ownership to display service

--- src/climate/display/CrowPanelDisplayService.h
+++ src/climate/display/CrowPanelDisplayService.h
@@ -2,7 +2,6 @@
 
 #include "climate/display/CrowPanelSsd1680DisplayBackend.h"
 #include "climate/display/DisplayAsyncTransaction.h"
-#include "climate/display/DisplayRenderAdapter.h"
 #include "climate/display/DisplayTelemetryObserver.h"
 
 #include <freertos/FreeRTOS.h>
@@ -11,6 +10,7 @@
 
 #include <array>
 #include <atomic>
+#include <cstddef>
 #include <cstdint>
 #include <type_traits>
 
@@ -19,6 +19,9 @@ namespace growbox::app::climate_io::display {
 struct CrowPanelDisplayServiceStatus final {
   bool started{false};
   bool render_in_flight{false};
+  bool clay_arena_in_psram{false};
+  std::uint32_t clay_arena_required_bytes{0U};
+  std::uint32_t clay_arena_allocated_bytes{0U};
   std::uint32_t submitted{0U};
   std::uint32_t submit_failures{0U};
   std::uint32_t render_successes{0U};
@@ -34,10 +37,9 @@ struct CrowPanelDisplayServiceStatus final {
 class CrowPanelDisplayService final {
 public:
   CrowPanelDisplayService(DisplayTelemetryObserver& observer,
-                          CrowPanelSsd1680DisplayBackend& backend, const DisplayTheme& theme = {},
+                          CrowPanelSsd1680DisplayBackend& backend,
                           std::uint64_t retry_backoff_ms = 30'000U) noexcept
-      : observer_(observer), backend_(backend), geometry_(observer.geometry()), theme_(theme),
-        retry_backoff_ms_(retry_backoff_ms) {}
+      : observer_(observer), backend_(backend), retry_backoff_ms_(retry_backoff_ms) {}
 
   CrowPanelDisplayService(const CrowPanelDisplayService&) = delete;
   CrowPanelDisplayService& operator=(const CrowPanelDisplayService&) = delete;
@@ -59,14 +61,13 @@ private:
   void observeStackWatermark() noexcept;
   void drainCompletions(std::uint64_t now_ms) noexcept;
   void submitPending(std::uint64_t now_ms) noexcept;
+  void releaseClayArena() noexcept;
 
   static_assert(std::is_trivially_copyable_v<DisplayRenderWorkItem>);
   static_assert(std::is_trivially_copyable_v<DisplayRenderCompletion>);
 
   DisplayTelemetryObserver& observer_;
   CrowPanelSsd1680DisplayBackend& backend_;
-  DisplayRenderGeometry geometry_{};
-  DisplayTheme theme_{};
   std::uint64_t retry_backoff_ms_{30'000U};
   std::uint64_t next_submit_ms_{0U};
   DisplayAsyncTransaction transaction_{};
@@ -80,6 +81,9 @@ private:
   QueueHandle_t render_queue_{nullptr};
   QueueHandle_t completion_queue_{nullptr};
   TaskHandle_t task_{nullptr};
+  std::uint8_t* clay_arena_{nullptr};
+  std::size_t clay_arena_required_bytes_{0U};
+  std::size_t clay_arena_bytes_{0U};
   bool started_{false};
 
   std::atomic<std::uint32_t> submitted_{0U};
